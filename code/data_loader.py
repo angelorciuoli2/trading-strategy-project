faf976056fadb0d74dd9de853e430234b8fd3e60
Angelo Orciuoli <168463684+angelorciuoli2@users.noreply.github.com>
2024-12-18 06:41:20 -0500

    Update data_loader.py

--- code/data_loader.py
+++ code/data_loader.py
@@ -5,7 +5,7 @@ class DataLoader:
     def load_data(file_path):
         df = pd.read_csv(file_path)
         df['Datetime'] = pd.to_datetime(df['Datetime'])
-        df = df[df['Datetime'].dt.second == 0]
-        df.reset_index(drop=True, inplace=True)
-        df.set_index('Datetime', inplace=True)
+        #df = df[df['Datetime'].dt.second == 0]
+        #df.reset_index(drop=True, inplace=True)
+        #df.set_index('Datetime', inplace=True)
         return df

9ff55ae1656251a11015fc67feebe323b868ccb5
Angelo Orciuoli <168463684+angelorciuoli2@users.noreply.github.com>
2025-03-05 20:34:22 -0500

    Delete code directory

--- code/ema.py
+++
@@ -1,6 +0,0 @@
-from ma_strategy import MovingAverageStrategy
-
-class EMA(MovingAverageStrategy):
-    def calculate_short_long(self):
-        self.df['Short'] = self.df[self.company].ewm(span=self.short_span, adjust=False).mean()
-        self.df['Long'] = self.df[self.company].ewm(span=self.long_span, adjust=False).mean()
--- code/sma.py
+++
@@ -1,6 +0,0 @@
-from ma_strategy import MovingAverageStrategy
-
-class SMA(MovingAverageStrategy):
-    def calculate_short_long(self):
-        self.df['Short'] = self.df[self.company].rolling(window=self.short_span, min_periods=1).mean()
-        self.df['Long'] = self.df[self.company].rolling(window=self.long_span, min_periods=1).mean()
